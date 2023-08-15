26487368d7419335cbdccff94952d83c1aa0bf1d
bahiru-kemer <bmulugeta@kifiya.et>
2023-08-15 13:28:36 +0300

    Create a web application of the blog

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + TypeScript + Vite + React Query + Tailwind css
+## The Web Application of my Blog System
+Get started with react, typescript, and react query
+
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
